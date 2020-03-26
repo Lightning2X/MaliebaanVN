ef2d1820465665a4106721ff84c3a5954ec1fa66
Schagen,T. (Tim) <t.schagen@students.uu.nl>
2020-03-26 16:35:42 +0100

    Add ability to toggle map and reorganize some files

---
+++ Assets/Src/UI/MapButtonHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapButtonHandler : MonoBehaviour
+{
+    public GameObject map, blur;
+
+    public void Clicked()
+    {
+        if (map.activeSelf)
+            CloseMap();
+        else
+            OpenMap();
+    }
+
+    private void OpenMap()
+    {
+        map.SetActive(true);
+        blur.SetActive(true);
+    }
+
+    private void CloseMap()
+    {
+        map.SetActive(false);
+        blur.SetActive(false);
+    }
+}
