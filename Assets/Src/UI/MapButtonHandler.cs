6b478b9595d544bcfa777d6dac1722607c506024
Schagen,T. (Tim) <t.schagen@students.uu.nl>
2020-03-27 13:23:08 +0100

    Remove DialogueHandler from scene

--- Assets/Src/UI/MapButtonHandler.cs
+++ Assets/Src/UI/MapButtonHandler.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MapButtonHandler : MonoBehaviour
 {
     public GameObject map, blur;
-    public DialogueHandler dialogueHandler;
     public void Clicked()
     {
         if (map.activeSelf)
@@ -18,13 +17,11 @@ public class MapButtonHandler : MonoBehaviour
     {
         map.SetActive(true);
         blur.SetActive(true);
-        dialogueHandler.DisableInput();
     }
 
     private void CloseMap()
     {
         map.SetActive(false);
         blur.SetActive(false);
-        dialogueHandler.EnableInput();
     }
 }
