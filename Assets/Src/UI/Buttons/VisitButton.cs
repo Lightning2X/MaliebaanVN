64ce714e8aaa3e9c61107fb5da1f62d76acb2a7a
Schagen,T. (Tim) <t.schagen@students.uu.nl>
2020-04-14 17:20:22 +0200

    Update credits scene again

---
+++ Assets/Src/UI/Buttons/VisitButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitButton : MonoBehaviour
+{
+    public void Clicked()
+    {
+        Application.OpenURL("https://aandemaliebaan.nl");
+    }
+}
